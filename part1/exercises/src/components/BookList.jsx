99d6e19e83a372452b23cc5c0fe6a307af3e5f71
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-08-22 14:06:15 -0500

    chores and hobbies half part done

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,17 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "Latest Book Releases";
+  let book1 = "https://www.goodreads.com/book/show/62628727-romantic-comedy";
+  let book2 =
+    "https://www.dragonsteelbooks.com/products/tress-of-the-emerald-sea-premium-hardcover";
+  let book3 =
+    "https://www.goodreads.com/book/show/61109069-the-london-s-ance-society";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" />
+      <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" />
+      <img src={book3} alt="The London Seance Society by Sarah Penner" />
+    </div>
+  );
 }
