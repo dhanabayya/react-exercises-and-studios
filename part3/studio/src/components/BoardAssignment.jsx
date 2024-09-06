d63dd9e808de0e3980bfccbfed4b7353ccdd7a1f
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-09-05 21:05:06 -0500

    studio part

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const boards = [
+    { label: "cottage", value: "cottagecore" },
+    { label: "yum", value: "yummy" },
+    { label: "notSharing", value: "I'm not sharing!" },
+  ];
+  const [boardName, setName] = useState("no boards yet!");
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
-      <select value={boardName} onChange={handleChange}>
-      </select>
-
+      <select value={boardName} onChange={handleChange}></select>
+      {boards.map((board) => (
+        <option key={board.value} value={board.value}>
+          {board.label}
+        </option>
+      ))}
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
